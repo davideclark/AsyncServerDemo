5f9d9d2122222efdadcd3e3abb980aac9aeeeecc
David Clark <david.e.clark@ntlworld.com>
2020-03-25 22:12:21 +0000

    Add project files.

---
+++ AsyncDemp/Controllers/DemoAsyncController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncDemp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoAsyncController : Controller
+    {
+		[HttpGet]
+		public async Task<string> Get()
+		{
+			HttpClient httpClient = new HttpClient();
+			
+			var result = await httpClient.GetAsync(@"https://localhost:44343/weatherforecastasync");
+
+			return await result.Content.ReadAsStringAsync();
+		}
+	}
+}
---
+++ AsyncDemp/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncDemp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : Controller
+    {
+		[HttpGet]
+		public string Get()
+		{
+			HttpResponseMessage response;
+			HttpClient httpClient = new HttpClient();
+			try
+			{
+				response = httpClient.GetAsync(@"https://localhost:44343/weatherforecastAsync").Result;
+			}
+			catch(Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine(ex.Message);
+				Console.WriteLine(ex.Message);
+				return "Http error " + ex.Message;
+			}
+
+			return response.Content.ReadAsStringAsync().Result;
+		}
+	}
+}
